151dac02e487b833f7f255d604af75bff3e2a3e0
Vittorio Migliore <vittorio.migliore@gmail.com>
2024-05-19 13:42:22 +0200

    body verse by 'Triangulation'

--- src/twelveDaysXmasSong.ts
+++ src/twelveDaysXmasSong.ts
@@ -26,6 +26,36 @@ export default class TwelveDaysXmasSong {
   verseBody(verse: number): string {
     var result = "";
     for(var i = verse; i>0; i--){
+        if(i == 12){
+            result += 'Twelve drummers drumming'+'\n'
+        }
+        if(i == 11){
+            result += 'Eleven pipers piping'+'\n'
+        }
+        if(i == 10){
+            result += 'Ten lords a-leaping'+'\n'
+        }
+        if(i == 9){
+            result += 'Nine ladies dancing'+'\n'
+        }
+        if(i == 8){
+            result += 'Eight maids a-milking'+'\n'
+        }
+        if(i == 7){
+            result += 'Seven swans a-swimming'+'\n'
+        }
+        if(i == 6){
+            result += 'Six geese a-laying'+'\n'
+        }
+        if(i == 5){
+            result += 'Five golden rings'+'\n'
+        }
+        if(i == 4){
+            result += 'Four calling birds'+'\n'
+        }
+        if(i == 3){
+            result += 'Three french hens'+'\n'
+        }
         if(i == 2){
             result += 'Two turtle doves and'+'\n'
         }
